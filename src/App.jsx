e36b4fb1f0979cca1cbddb4442a5b46c45421749
chamalg <chamal.gunasinghe@gmail.com>
2025-04-22 19:22:12 +0530

    Modified code

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import PowerButton from './PowerButton';
 import FullscreenOverlay from './FullScreenOverlay';
 import FlipCard from './FlipCard';
