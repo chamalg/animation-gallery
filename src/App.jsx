f282e798859892d748baec35b8112542200a9142
chamalg <chamal.gunasinghe@gmail.com>
2025-04-22 20:09:24 +0530

    Modified code

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PowerButton from './PowerButton';
-import FullScreenOverlay from './FullScreenOverlay';
+import FullScreenOverlay from "./FullScreenOverlay";
 import FlipCard from './FlipCard';
 import './App.css';
 import CodeEditor from './CodeEditor';
